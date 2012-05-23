5ee235a379651718536c23a488ce4cc438426e23
Arin Sime <asime@o19s.com>
2012-05-23 07:28:59 -0400

    first rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wishlisting::Application.config.secret_token = '4087020ab199e45e71f09ea006d94647cb2d549dcc08593dda940290082664eb39c0eba616dea4d9cb1945fd3c28ce731e088b49b9b4bd058a5ec4b81d445160'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wishlisting::Application.config.session_store :cookie_store, key: '_wishlisting_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wishlisting::Application.config.session_store :active_record_store
