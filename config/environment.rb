1875ab1d91363584d5b3bfe5d2d2ddbfde7ec90b
asime <arin@arinsime.com>
2012-06-11 18:49:59 -0400

    removing env changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,32 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Wishlisting::Application.initialize!
+
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+
+require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
+require 'cucumber/rails/world'
+require 'cucumber/rails/active_record'
+require 'cucumber/web/tableish'
+
+require 'capybara/rails'
+require 'capybara/cucumber'
+require 'capybara/session'
+require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
+
+Capybara.default_selector = :css
+
+ActionController::Base.allow_rescue = false
+
+
+Cucumber::Rails::World.use_transactional_fixtures = true
+
+if defined?(ActiveRecord::Base)
+  begin
+    require 'database_cleaner'
+    DatabaseCleaner.strategy = :truncation
+  rescue LoadError => ignore_if_database_cleaner_not_present
+  end
+end
+
