f78eb86383c19497ed9e7dfe8cab9efd38a4e828
ConferenceDev1 <dev1@agilityfeat.com>
2012-08-13 18:03:43 +0200

    testing cloudinary upload

--- features/image_upload.feature
+++ features/image_upload.feature
@@ -7,6 +7,6 @@ Scenario:  Display sample image from Cloudinary
 
 @cloudinary @wip
 Scenario: Upload an image to cloudinary
-	Given I have a local file named "rails.png"
+	Given I have a local file named "app/assets/images/rails.png"
 	When I do a direct upload to cloudinary
 	Then I should be able to view that image in the browser at "http://res.cloudinary.com/wishlisting-org/image/upload/rails.png"
--- features/step_definitions/image_upload_steps.rb
+++ features/step_definitions/image_upload_steps.rb
@@ -5,3 +5,17 @@ end
 Then /^I should see 'sample\.jpg' from Cloudinary$/ do
   page.should have_xpath("//img[contains(@src, \"sample.jpg\")]")
 end
+
+Given /^I have a local file named "(.*?)"$/ do |filename|
+  File.exists?(filename).should == true
+  @localfile = filename
+end
+
+When /^I do a direct upload to cloudinary$/ do
+  Cloudinary::Uploader.upload(File.open(@localfile))
+end
+
+Then /^I should be able to view that image in the browser at "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
